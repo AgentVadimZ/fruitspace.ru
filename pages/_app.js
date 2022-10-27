5fd4985410084565da17974d674d62581462b0e0
M41den <38406005+cradio@users.noreply.github.com>
2022-10-27 10:49:35 +0300

    Fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import '../styles/globals.css'
 import {StyledEngineProvider} from "@mui/material";
+import {RecoilRoot} from "recoil";
 
 export default function WebApp({ Component, pageProps }) {
   return (
+      <RecoilRoot>
           <StyledEngineProvider injectFirst>
-            <Component {...pageProps} />
+              <Component {...pageProps} />
           </StyledEngineProvider>
+      </RecoilRoot>
       )
 
 }
