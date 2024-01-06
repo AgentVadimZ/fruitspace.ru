a1dbcf4a81e2967156613843a34e81c027b65ff5
Leapher <118823348+Leapher@users.noreply.github.com>
2024-01-07 00:38:32 +0300

    m41den said my buttons was bad. me disagree, m41den canceled my commits. me did the buttons again. with tailwind. me happy

--- pages/product/gd.jsx
+++ pages/product/gd.jsx
@@ -6,6 +6,7 @@ import BannerGD from "../../components/assets/BannerGD.png"
 import ProductHeader from "../../components/Global/ProductHeader";
 import ProductCardGD from "../../components/Cards/ProductCardGD";
 import {
+    Button,
     ListItem,
     ListItemIcon,
     ListItemText,
@@ -33,6 +34,8 @@ import FeatureShieldImg from "../../components/assets/features/shield_3d-sm.png"
 import FeatureSetupImg from "../../components/assets/features/autosetup_3d-sm.png"
 import FeatureMusicImg from "../../components/assets/features/music_3d-sm.png"
 import useLocale, {useGlobalLocale} from "../../locales/useLocale";
+import discordLogo from "../../components/assets/social/discord.png";
+import GitHubIcon from "../../components/assets/logos/GithubIcon.png";
 
 
 
@@ -193,9 +196,20 @@ export default function GD(props) {
                         </TableBody>
                     </Table>
                 </TableContainer>
-                <p style={{margin:"1rem auto", maxWidth:"320px"}}>
-                    {locale.get('selfHostNotDone')}
+                <p className="mx-auto text-center max-w-2xl">
+                    {locale.get('selfHostLinks')}
                 </p>
+                <div className="flex mx-auto gap-2 justify-center mb-2">
+                <Button variant="contained" className="m-2 mt-auto font-bold rounded-lg bg-[#6e5494] h-12 hover:bg-[#5f4980]" onClick={() => window.location.href = "https://github.com/FruitSpace/HalogenGDPSCore"}>
+                    <img className="invert h-10 mr-3 h-3/4 rounded" src={GitHubIcon.src} alt="github"/>
+                    HalogenCore Github
+                </Button>
+                <Button variant="contained" className="m-2 mt-auto font-bold rounded-lg bg-[#7289da] h-12 hover:bg-[#5f73b8]"  onClick={() => window.location.href = "https://discord.gg/HgBQmMRKTB"}>
+                    <img className="invert h-10 mr-3 h-3/4" src={discordLogo.src} alt="discord"/>
+                    HalogenCore Discord
+                </Button>
+
+                </div>
 
             </div>
             <Footer router={props.router}/>
