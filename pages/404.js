ff4b9c53f42ca8dc6294b4007087872d9ae8268e
M41den <38406005+m41denx@users.noreply.github.com>
2024-01-06 20:08:17 +0300

    [run] Blook fix

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,10 @@
+import Link from "next/link";
 
 
 export default function NotFoundPage(props) {
     return <div className="h-screen flex flex-col items-center justify-center">
         <h1 className="text-5xl xl:text-8xl font-[Coolvetica] tracking-wider font-normal fruitText m-0 select-none">404</h1>
-        <p>Тут ничего нет, может вы искали <a href="/" className="text-blue-600 hover:underline">Главную страницу</a>?</p>
+        <p>Тут ничего нет, может вы искали <Link href="/" className="text-blue-600 hover:underline">Главную страницу</Link>?</p>
     </div>
 
 }
