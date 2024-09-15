8175cd1fbff522b98b974e9c25b9e05dbbd8f135
KilDoom <117968565+KilDoomWise@users.noreply.github.com>
2024-09-15 12:07:28 +0300

    Update ProductOrder.tsx

--- components/Extra/ProductOrder.tsx
+++ components/Extra/ProductOrder.tsx
@@ -1,7 +1,15 @@
 import React, {useState} from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faXmark, faArrowLeft, faArrowRight, faCheckCircle, faFlag} from "@fortawesome/free-solid-svg-icons";
+import {
+    faXmark,
+    faArrowLeft,
+    faArrowRight,
+    faCheckCircle,
+    faFlag,
+    faMicrochip, faMemory, faHardDrive
+} from "@fortawesome/free-solid-svg-icons";
 import {Button, Segmented} from "antd";
+import ProductCardMC from "@/components/Cards/ProductCardMC";
 
 
 const MinecraftOrderModal = (props: any) => {
@@ -16,11 +24,17 @@ const MinecraftOrderModal = (props: any) => {
             <div className="w-full ipad:w-4/5 flex flex-col ipad:flex-row gap-8 h-[75%]"
                  onClick={(e) => e.stopPropagation()}>
                 <div className="bg-active glassb p-4 rounded-2xl flex-1 flex flex-col">
-                    <p>Я еблан тут страница 1</p>
+                    {page == 0 && (
+                        <div>
+                            <h1 className="text-xl">Создание Minecraft сервера</h1>
+
+                        </div>
+                    )}
                     <div className="mt-auto flex items-center justify-end gap-8">
                         <div className="flex justify-center items-center gap-2 flex-1">
                             <FontAwesomeIcon icon={faFlag}/>
                             {Array.from({length: maxPages}, (_, i) => i).map((i,v)=>{
+                                return <div key={i} className={`rounded-full h-2 w-16 ${i<=page?"bg-success":"bg-btn"}`} />
                             })}
                             <FontAwesomeIcon icon={faCheckCircle}/>
                         </div>
@@ -34,11 +48,12 @@ const MinecraftOrderModal = (props: any) => {
                         <h1>
                             Horizon
                         </h1>
-                        <hr className="border-1 glassb mt-1 mb-1"/>
                         <p className="text-right">
                             2599₽/мес
                         </p>
+                        <hr className="border-1 glassb mt-1 mb-1"/>
                     </div>
+
                 </div>
             </div>
         </div>
@@ -77,6 +92,14 @@ export function ProductOrder({closeModal}: { closeModal: () => void }) {
         </div>
     </div>
 }
+
+const corePrint = (n) => {
+    n%=10
+    if (n==1) return "ядро"
+    if (n<1 || 1<n && n<5) return "ядра"
+    return "ядер"
+}
+
 
 const tariffs = {}
 tariffs.dynamic = [
@@ -141,52 +164,3 @@ tariffs.dynamic = [
         ssd: 100
     },
 ]
-
-tariffs.static = [
-    {
-        title: "Air",
-        id: "S-1 S+",
-        price: 550,
-        cpus: 1,
-        maxRam: 4,
-        ssd: 30
-    },
-    {
-        title: "Viper",
-        id: "S-2 M+",
-        price: 1000,
-        cpus: 2,
-        maxRam: 8,
-        ssd: 40
-    },
-    {
-        title: "Carbon",
-        id: "S-3 L+",
-        price: 1500,
-        cpus: 3,
-        maxRam: 12,
-        ssd: 60
-    },
-    {
-        title: "Proton",
-        id: "S-4 XL+",
-        price: 2300,
-        cpus: 4,
-        maxRam: 16,
-        ssd: 80
-    },
-    {
-        title: "Warp",
-        id: "S-5 XXL+",
-        price: 3200,
-        cpus: 5,
-        maxRam: 24,
-        ssd: 120
-    },
-]
-const corePrint = (n) => {
-    n %= 10
-    if (n == 1) return "ядро"
-    if (n < 1 || 1 < n && n < 5) return "ядра"
-    return "ядер"
-}
